c71ac9ce1a242a004a0e7a422090a1d86185131a
poojamadiwalar449@gmail.com <poojamadiwalar449@gmail.com>
2025-05-06 15:15:58 +0530

    message

---
+++ BasicOperations.java
@@ -0,0 +1,18 @@
+
+public class BasicOperations {
+
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 5;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Addition" + a + ": " + sum);
+        System.out.println("subtraction" + b + ": " + difference);
+        System.out.println("Multiplication" + a + ": " + product);
+    }
+
+}
