e188b3ef6b5cff85f3f73ac4b8f45c75395e5ef9
poojamadiwalar449@gmail.com <poojamadiwalar449@gmail.com>
2025-05-06 15:18:18 +0530

    message

--- BasicOperations.java
+++ BasicOperations.java
@@ -10,9 +10,6 @@ public class BasicOperations {
         System.out.println("Sum: " + sum);
         System.out.println("Difference: " + difference);
         System.out.println("Product: " + product);
-        System.out.println("Addition" + a + ": " + sum);
-        System.out.println("subtraction" + b + ": " + difference);
-        System.out.println("Multiplication" + a + ": " + product);
     }
 
 }
